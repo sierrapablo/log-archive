b5769068e833bf2bc1fe49332f5df501be4bdc0e
sierrapablo <pablosierralorente@outlook.es>
2025-04-04 14:28:57 +0200

    🔧 improvement: Translation to english

--- src/main.rs
+++ src/main.rs
@@ -14,32 +14,32 @@ use tar::Builder;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        eprintln!("Uso: log-archive <directorio-de-logs>");
+        eprintln!("Use: log-archive <logs-directory>");
         process::exit(1);
     }
 
     let log_dir = Path::new(&args[1]);
     if !log_dir.exists() || !log_dir.is_dir() {
-        eprintln!("Error: '{}' no es un directorio válido.", log_dir.display());
+        eprintln!("Error: '{}' is not a valid directory.", log_dir.display());
         process::exit(1);
     }
 
     let archive_dir = Path::new("archived_logs");
     if let Err(e) = fs::create_dir_all(&archive_dir) {
-        eprintln!("Error al crear directorio de archivos: {}", e);
+        eprintln!("Error creating directory: {}", e);
         process::exit(1);
     }
 
     match create_archive(log_dir, archive_dir) {
         Ok(archive_path) => {
             if let Err(e) = log_event(&archive_path, archive_dir) {
-                eprintln!("Error al registrar evento: {}", e);
+                eprintln!("Error registering event: {}", e);
             } else {
-                println!("✔️ Archivo creado: {}", archive_path.display());
+                println!("✔️ File successfully created: {}", archive_path.display());
             }
         }
         Err(e) => {
-            eprintln!("Error al crear archivo: {}", e);
+            eprintln!("Error creating the file: {}", e);
             process::exit(1);
         }
     }
@@ -69,7 +69,7 @@ fn log_event(archive_path: &Path, archive_dir: &Path) -> io::Result<()> {
     let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
     writeln!(
         file,
-        "[{}] Archivo creado: {}",
+        "[{}] File created: {}",
         timestamp,
         archive_path.file_name().unwrap().to_string_lossy()
     )?;
